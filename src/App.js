4065afb005694780c70771f624f0e045a60c7024
Su Ho V <su.ho@asiantech.vn>
2018-03-09 08:04:48 +0700

    flat css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import './App.css'
+import Flat from './components/flat';
 
 class App extends Component {
   render() {
+    const flat = {
+      "name": "Test Name 2",
+      "imageURL": "https://raw.githubusercontent.com/suhv/devday-react-demo/master/resources/flat1.png",
+      "price": 10,
+      "lat": 48.884211,
+      "lng": 2.345890
+    }
     return (
       <div className='app'>
         <div className='main'>
           <div className='search'></div>
-          <div className='flats'></div>
+          <div className='flats'>
+            <Flat flat={flat} />
+            <Flat flat={flat} />
+            <Flat flat={flat} />
+            <Flat flat={flat} />
+          </div>
         </div>
         <div className='map'></div>
       </div>
