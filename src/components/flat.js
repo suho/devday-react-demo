beafa7b4b8471208787d5165799b1998e8d61c43
Su Ho V <su.ho@asiantech.vn>
2018-03-09 07:42:14 +0700

    flat

--- src/components/flat.js
+++ src/components/flat.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import './flat.css';
 
 class Flat extends Component {
     render() {
+        const title = this.props.flat.price + '$ - ' + this.props.flat.name;
+        const style = { backgroundImage: `url(${this.props.flat.imageURL})` }
         return (
             <div className='flat'>
-                <div className='flat-picture'></div>
-                <div className='flat-title'></div>
+                <div className='flat-picture' style={style}></div>
+                <div className='flat-title'>
+                    {title}
+                </div>
             </div>
         );
     }
