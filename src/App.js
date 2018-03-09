76e7706c98272cda47d0f01aa62a38f38e0454a1
Su Ho V <su.ho@asiantech.vn>
2018-03-09 07:44:36 +0700

    flexbox

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import Flat from './components/flat';
 
 class App extends Component {
   render() {
-    const flat = {
-      "name": "Test Name 2",
-      "imageURL": "https://raw.githubusercontent.com/suhv/devday-react-demo/master/resources/flat1.png",
-      "price": 10,
-      "lat": 48.884211,
-      "lng": 2.345890
-    }
     return (
       <div>
-        <Flat flat={flat} />
+        <div className='app'>
+          <div className='main'>
+            <div className='search'></div>
+            <div className='flats'></div>
+          </div>
+          <div className='map'></div>
+        </div>
       </div>
     );
   }
